22e9b8d1867fe59cca14cbd5057ee30b17857b76
DiegoLara-FlagerCraft <laramnbvo123@gmail.com>
2021-10-10 23:05:22 -0500

    TERMINADO

--- BlackJack21/Form1.Designer.cs
+++ BlackJack21/Form1.Designer.cs
@@ -31,8 +31,6 @@ namespace BlackJack21
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.RtxtManoD = new System.Windows.Forms.RichTextBox();
-            this.BtnvCarta = new System.Windows.Forms.Button();
-            this.RtxtMostrar = new System.Windows.Forms.RichTextBox();
             this.BtnIniciar = new System.Windows.Forms.Button();
             this.LabelD = new System.Windows.Forms.Label();
             this.LabelP = new System.Windows.Forms.Label();
@@ -43,6 +41,12 @@ namespace BlackJack21
             this.RtxtScoreD = new System.Windows.Forms.RichTextBox();
             this.RtxtScoreP = new System.Windows.Forms.RichTextBox();
             this.LabelScoreP = new System.Windows.Forms.Label();
+            this.LabelGames = new System.Windows.Forms.Label();
+            this.RtxtGames = new System.Windows.Forms.RichTextBox();
+            this.LabelWins = new System.Windows.Forms.Label();
+            this.LabelFails = new System.Windows.Forms.Label();
+            this.RtxtWins = new System.Windows.Forms.RichTextBox();
+            this.RtxtFails = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // RtxtManoD
@@ -50,39 +54,22 @@ namespace BlackJack21
             this.RtxtManoD.BackColor = System.Drawing.Color.Green;
             this.RtxtManoD.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.RtxtManoD.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RtxtManoD.Location = new System.Drawing.Point(541, 46);
+            this.RtxtManoD.Location = new System.Drawing.Point(496, 46);
             this.RtxtManoD.Name = "RtxtManoD";
+            this.RtxtManoD.ReadOnly = true;
             this.RtxtManoD.Size = new System.Drawing.Size(166, 112);
             this.RtxtManoD.TabIndex = 2;
             this.RtxtManoD.Text = "";
-            // 
-            // BtnvCarta
-            // 
-            this.BtnvCarta.Location = new System.Drawing.Point(43, 224);
-            this.BtnvCarta.Name = "BtnvCarta";
-            this.BtnvCarta.Size = new System.Drawing.Size(131, 57);
-            this.BtnvCarta.TabIndex = 0;
-            this.BtnvCarta.Text = "Add";
-            this.BtnvCarta.UseVisualStyleBackColor = true;
-            this.BtnvCarta.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // RtxtMostrar
-            // 
-            this.RtxtMostrar.Location = new System.Drawing.Point(3, 12);
-            this.RtxtMostrar.Name = "RtxtMostrar";
-            this.RtxtMostrar.Size = new System.Drawing.Size(237, 196);
-            this.RtxtMostrar.TabIndex = 1;
-            this.RtxtMostrar.Text = "";
             // 
             // BtnIniciar
             // 
             this.BtnIniciar.BackColor = System.Drawing.Color.DarkGreen;
             this.BtnIniciar.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnIniciar.Location = new System.Drawing.Point(541, 216);
+            this.BtnIniciar.Location = new System.Drawing.Point(496, 216);
             this.BtnIniciar.Name = "BtnIniciar";
             this.BtnIniciar.Size = new System.Drawing.Size(166, 65);
             this.BtnIniciar.TabIndex = 3;
-            this.BtnIniciar.Text = "Iniciar";
+            this.BtnIniciar.Text = "JUGAR";
             this.BtnIniciar.UseVisualStyleBackColor = false;
             this.BtnIniciar.Click += new System.EventHandler(this.Iniciar_Click);
             // 
@@ -91,7 +78,7 @@ namespace BlackJack21
             this.LabelD.AutoSize = true;
             this.LabelD.BackColor = System.Drawing.Color.Transparent;
             this.LabelD.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelD.Location = new System.Drawing.Point(560, 12);
+            this.LabelD.Location = new System.Drawing.Point(515, 12);
             this.LabelD.Name = "LabelD";
             this.LabelD.Size = new System.Drawing.Size(125, 31);
             this.LabelD.TabIndex = 4;
@@ -102,7 +89,7 @@ namespace BlackJack21
             this.LabelP.AutoSize = true;
             this.LabelP.BackColor = System.Drawing.Color.Transparent;
             this.LabelP.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelP.Location = new System.Drawing.Point(560, 334);
+            this.LabelP.Location = new System.Drawing.Point(515, 334);
             this.LabelP.Name = "LabelP";
             this.LabelP.Size = new System.Drawing.Size(124, 31);
             this.LabelP.TabIndex = 5;
@@ -113,8 +100,9 @@ namespace BlackJack21
             this.RtxtManoP.BackColor = System.Drawing.Color.Green;
             this.RtxtManoP.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.RtxtManoP.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RtxtManoP.Location = new System.Drawing.Point(541, 385);
+            this.RtxtManoP.Location = new System.Drawing.Point(496, 385);
             this.RtxtManoP.Name = "RtxtManoP";
+            this.RtxtManoP.ReadOnly = true;
             this.RtxtManoP.Size = new System.Drawing.Size(166, 112);
             this.RtxtManoP.TabIndex = 6;
             this.RtxtManoP.Text = "";
@@ -123,18 +111,19 @@ namespace BlackJack21
             // 
             this.BtnPlantar.BackColor = System.Drawing.Color.DarkGreen;
             this.BtnPlantar.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnPlantar.Location = new System.Drawing.Point(455, 513);
+            this.BtnPlantar.Location = new System.Drawing.Point(410, 513);
             this.BtnPlantar.Name = "BtnPlantar";
             this.BtnPlantar.Size = new System.Drawing.Size(137, 45);
             this.BtnPlantar.TabIndex = 7;
             this.BtnPlantar.Text = "Plantar";
             this.BtnPlantar.UseVisualStyleBackColor = false;
+            this.BtnPlantar.Click += new System.EventHandler(this.BtnPlantar_Click);
             // 
             // BtnPedir
             // 
             this.BtnPedir.BackColor = System.Drawing.Color.DarkGreen;
             this.BtnPedir.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnPedir.Location = new System.Drawing.Point(652, 513);
+            this.BtnPedir.Location = new System.Drawing.Point(607, 513);
             this.BtnPedir.Name = "BtnPedir";
             this.BtnPedir.Size = new System.Drawing.Size(137, 45);
             this.BtnPedir.TabIndex = 8;
@@ -147,7 +136,7 @@ namespace BlackJack21
             this.LabelScoreD.AutoSize = true;
             this.LabelScoreD.BackColor = System.Drawing.Color.Transparent;
             this.LabelScoreD.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelScoreD.Location = new System.Drawing.Point(410, 46);
+            this.LabelScoreD.Location = new System.Drawing.Point(365, 46);
             this.LabelScoreD.Name = "LabelScoreD";
             this.LabelScoreD.Size = new System.Drawing.Size(99, 22);
             this.LabelScoreD.TabIndex = 9;
@@ -158,8 +147,9 @@ namespace BlackJack21
             this.RtxtScoreD.BackColor = System.Drawing.Color.Green;
             this.RtxtScoreD.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.RtxtScoreD.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RtxtScoreD.Location = new System.Drawing.Point(414, 71);
+            this.RtxtScoreD.Location = new System.Drawing.Point(369, 71);
             this.RtxtScoreD.Name = "RtxtScoreD";
+            this.RtxtScoreD.ReadOnly = true;
             this.RtxtScoreD.Size = new System.Drawing.Size(92, 58);
             this.RtxtScoreD.TabIndex = 10;
             this.RtxtScoreD.Text = "";
@@ -169,8 +159,9 @@ namespace BlackJack21
             this.RtxtScoreP.BackColor = System.Drawing.Color.Green;
             this.RtxtScoreP.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.RtxtScoreP.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RtxtScoreP.Location = new System.Drawing.Point(414, 410);
+            this.RtxtScoreP.Location = new System.Drawing.Point(369, 410);
             this.RtxtScoreP.Name = "RtxtScoreP";
+            this.RtxtScoreP.ReadOnly = true;
             this.RtxtScoreP.Size = new System.Drawing.Size(92, 58);
             this.RtxtScoreP.TabIndex = 11;
             this.RtxtScoreP.Text = "";
@@ -180,11 +171,80 @@ namespace BlackJack21
             this.LabelScoreP.AutoSize = true;
             this.LabelScoreP.BackColor = System.Drawing.Color.Transparent;
             this.LabelScoreP.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelScoreP.Location = new System.Drawing.Point(410, 385);
+            this.LabelScoreP.Location = new System.Drawing.Point(365, 385);
             this.LabelScoreP.Name = "LabelScoreP";
             this.LabelScoreP.Size = new System.Drawing.Size(99, 22);
             this.LabelScoreP.TabIndex = 12;
             this.LabelScoreP.Text = "PUNTAJE";
+            // 
+            // LabelGames
+            // 
+            this.LabelGames.AutoSize = true;
+            this.LabelGames.BackColor = System.Drawing.Color.Transparent;
+            this.LabelGames.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelGames.Location = new System.Drawing.Point(797, 20);
+            this.LabelGames.Name = "LabelGames";
+            this.LabelGames.Size = new System.Drawing.Size(210, 22);
+            this.LabelGames.TabIndex = 13;
+            this.LabelGames.Text = "PARTIDAS JUGADAS";
+            // 
+            // RtxtGames
+            // 
+            this.RtxtGames.BackColor = System.Drawing.Color.Green;
+            this.RtxtGames.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RtxtGames.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RtxtGames.Location = new System.Drawing.Point(854, 46);
+            this.RtxtGames.Name = "RtxtGames";
+            this.RtxtGames.ReadOnly = true;
+            this.RtxtGames.Size = new System.Drawing.Size(92, 37);
+            this.RtxtGames.TabIndex = 14;
+            this.RtxtGames.Text = "";
+            // 
+            // LabelWins
+            // 
+            this.LabelWins.AutoSize = true;
+            this.LabelWins.BackColor = System.Drawing.Color.Transparent;
+            this.LabelWins.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelWins.Location = new System.Drawing.Point(797, 98);
+            this.LabelWins.Name = "LabelWins";
+            this.LabelWins.Size = new System.Drawing.Size(217, 22);
+            this.LabelWins.TabIndex = 15;
+            this.LabelWins.Text = "PARTIDAS GANADAS";
+            // 
+            // LabelFails
+            // 
+            this.LabelFails.AutoSize = true;
+            this.LabelFails.BackColor = System.Drawing.Color.Transparent;
+            this.LabelFails.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelFails.Location = new System.Drawing.Point(792, 170);
+            this.LabelFails.Name = "LabelFails";
+            this.LabelFails.Size = new System.Drawing.Size(215, 22);
+            this.LabelFails.TabIndex = 16;
+            this.LabelFails.Text = "PARTIDAS PERDIDAS";
+            // 
+            // RtxtWins
+            // 
+            this.RtxtWins.BackColor = System.Drawing.Color.Green;
+            this.RtxtWins.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RtxtWins.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RtxtWins.Location = new System.Drawing.Point(854, 123);
+            this.RtxtWins.Name = "RtxtWins";
+            this.RtxtWins.ReadOnly = true;
+            this.RtxtWins.Size = new System.Drawing.Size(92, 37);
+            this.RtxtWins.TabIndex = 17;
+            this.RtxtWins.Text = "";
+            // 
+            // RtxtFails
+            // 
+            this.RtxtFails.BackColor = System.Drawing.Color.Green;
+            this.RtxtFails.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RtxtFails.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RtxtFails.Location = new System.Drawing.Point(854, 195);
+            this.RtxtFails.Name = "RtxtFails";
+            this.RtxtFails.ReadOnly = true;
+            this.RtxtFails.Size = new System.Drawing.Size(92, 37);
+            this.RtxtFails.TabIndex = 18;
+            this.RtxtFails.Text = "";
             // 
             // Form1
             // 
@@ -192,6 +252,12 @@ namespace BlackJack21
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1019, 607);
+            this.Controls.Add(this.RtxtFails);
+            this.Controls.Add(this.RtxtWins);
+            this.Controls.Add(this.LabelFails);
+            this.Controls.Add(this.LabelWins);
+            this.Controls.Add(this.RtxtGames);
+            this.Controls.Add(this.LabelGames);
             this.Controls.Add(this.LabelScoreP);
             this.Controls.Add(this.RtxtScoreP);
             this.Controls.Add(this.RtxtScoreD);
@@ -203,8 +269,6 @@ namespace BlackJack21
             this.Controls.Add(this.LabelD);
             this.Controls.Add(this.BtnIniciar);
             this.Controls.Add(this.RtxtManoD);
-            this.Controls.Add(this.RtxtMostrar);
-            this.Controls.Add(this.BtnvCarta);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -214,8 +278,6 @@ namespace BlackJack21
 
         #endregion
         private System.Windows.Forms.RichTextBox RtxtManoD;
-        private System.Windows.Forms.Button BtnvCarta;
-        private System.Windows.Forms.RichTextBox RtxtMostrar;
         private System.Windows.Forms.Button BtnIniciar;
         private System.Windows.Forms.Label LabelD;
         private System.Windows.Forms.Label LabelP;
@@ -226,6 +288,12 @@ namespace BlackJack21
         private System.Windows.Forms.RichTextBox RtxtScoreD;
         private System.Windows.Forms.RichTextBox RtxtScoreP;
         private System.Windows.Forms.Label LabelScoreP;
+        private System.Windows.Forms.Label LabelGames;
+        private System.Windows.Forms.RichTextBox RtxtGames;
+        private System.Windows.Forms.Label LabelWins;
+        private System.Windows.Forms.Label LabelFails;
+        private System.Windows.Forms.RichTextBox RtxtWins;
+        private System.Windows.Forms.RichTextBox RtxtFails;
     }
 }
 
--- BlackJack21/Form1.cs
+++ BlackJack21/Form1.cs
@@ -18,8 +18,9 @@ namespace BlackJack21
         int ScoreP = 0;
         int Wins = 0;
         int Fails = 0;
-        int Games =  0;
-        
+        int Games = 0;
+        int Very = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -28,28 +29,44 @@ namespace BlackJack21
             BtnPlantar.Hide();
         }
 
-        public int Verif(List<Card> Hand)
+        public int Check(List<Card> Hand)
         {
+
             int Score = 0;
             foreach (Card c in Hand)
             {
-                if ( c.Score == 1 && Score + c.Score > 21)
+                if (c.score == 1 && Score + c.score <= 10)
+                {
+                    Score += 11;
+                }
+                else if (c.score == 1 && Score + c.score > 10)
                 {
                     Score += 1;
                 }
                 else
                 {
-                    Score += c.Score;
+                    Score += c.score;
                 }
             }
             return
                 Score;
 
         }
+
+        public void Clean()
+        {
+            P.Hand = new List<Card>();
+            D.Hand = new List<Card>();
+            RtxtManoD.Text = " ";
+            RtxtManoP.Text = " ";
+            RtxtScoreD.Text = " ";
+            RtxtScoreP.Text = " ";
+            ScoreD = 0;
+            ScoreP = 0;
+        }
         public void CardsP()
         {
             RtxtManoP.Clear();
-            RtxtScoreD.Clear();
             foreach (Card c in P.Hand)
             {
                 RtxtManoP.Text += c.Suit + c.Symbol + "\n";
@@ -58,10 +75,21 @@ namespace BlackJack21
         public void CardsD()
         {
             RtxtManoD.Clear();
+            Card c = D.Hand.First();
+            RtxtManoD.Text += c.Suit + c.Symbol + "\n";
+            ScoreD = c.score;
+            RtxtScoreD.Text = Convert.ToString(ScoreD);
+        }
+
+        public void CardsDfn()
+        {
+            RtxtManoD.Clear();
             foreach (Card c in D.Hand)
             {
                 RtxtManoD.Text += c.Suit + c.Symbol + "\n";
             }
+            ScoreD = Check(D.Hand);
+            RtxtScoreD.Text = Convert.ToString(ScoreD);
         }
 
         private void Iniciar_Click(object sender, EventArgs e)
@@ -73,45 +101,124 @@ namespace BlackJack21
             D.Init();
             Card card1 = D.Deal();
             Card card2 = D.Deal();
-            P.Init(card1,card2);
+            P.Init(card1, card2);
             CardsP();
             CardsD();
-            ScoreP = Verif(P.Hand);
+            ScoreP = Check(P.Hand);
             RtxtScoreP.Text = Convert.ToString(ScoreP);
+            Games += 1;
+            RtxtGames.Text = Convert.ToString(Games);
+
+            if (ScoreP == 21)
+            {
+                BtnPedir.Hide();
+                BtnPlantar.Hide();
+                MessageBox.Show("BLACKJACK");
+                BtnIniciar.Show();
+                Clean();
+                Wins += 1;
+                RtxtWins.Text = Convert.ToString(Wins);
+            }
+            
         }
         private void BtnPedir_Click(object sender, EventArgs e)
         {
             Card c = D.Deal();
             P.AddCard(c);
             CardsP();
-            ScoreP = Verif(P.Hand);
+            ScoreP = Check(P.Hand);
             RtxtScoreP.Text = Convert.ToString(ScoreP);
+
+            if (ScoreP > 21)
+            {
+                BtnPedir.Hide();
+                BtnPlantar.Hide();
+                MessageBox.Show("PERDISTE");
+                BtnIniciar.Show();
+                Clean();
+                Fails += 1;
+                RtxtFails.Text = Convert.ToString(Fails);
+            }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        private void button1_Click(object sender, EventArgs e)
+        private void BtnPlantar_Click(object sender, EventArgs e)
         {
-            Dealer d = new Dealer();
-            d.Generate();
+            CardsDfn();
+            ScoreD = Check(D.Hand);
 
-            foreach(Card c in d.GetDeck())
+            if (ScoreD >= ScoreP)
             {
-                RtxtMostrar.Text += c.Symbol + c.Suit + "- score: " + c.Score + " color: " + c.Color + "\n";
+                BtnPedir.Hide();
+                BtnPlantar.Hide();
+                MessageBox.Show("PERDISTE");
+                BtnIniciar.Show();
+                Clean();
+                Fails += 1;
+                RtxtFails.Text = Convert.ToString(Fails);
+            }
+
+            else if (ScoreP > 17)
+            {
+                while (ScoreD < 21)
+                {
+                    Card c = D.Deal();
+                    D.AddCard(c);
+                    CardsDfn();
+                    ScoreD = Check(D.Hand);
+                    RtxtScoreD.Text = Convert.ToString(ScoreD);
+                }
+            }
+
+            else if (ScoreP <= 17)
+            {
+                while (ScoreD < 17)
+                {
+                    Card c = D.Deal();
+                    D.AddCard(c);
+                    CardsDfn();
+                    ScoreD = Check(D.Hand);
+                    RtxtScoreD.Text = Convert.ToString(ScoreD);
+                }
+            }
+
+            Confirm();
+        }
+
+        public void Confirm()
+        {
+            if (ScoreD < ScoreP)
+            {
+                BtnPedir.Hide();
+                BtnPlantar.Hide();
+                MessageBox.Show("GANASTE");
+                BtnIniciar.Show();
+                Clean();
+                Wins += 1;
+                RtxtWins.Text = Convert.ToString(Wins);
+            }
+
+            else if (ScoreD > 21)
+            {
+                BtnPedir.Hide();
+                BtnPlantar.Hide();
+                MessageBox.Show("GANASTE");
+                BtnIniciar.Show();
+                Clean();
+                Wins += 1;
+                RtxtWins.Text = Convert.ToString(Wins);
+            }
+
+            else if (ScoreD > ScoreP && ScoreD <= 21)
+            {
+                BtnPedir.Hide();
+                BtnPlantar.Hide();
+                MessageBox.Show("PERDISTE");
+                BtnIniciar.Show();
+                Clean();
+                Fails += 1;
+                RtxtFails.Text = Convert.ToString(Fails);
             }
         }
     }
 }
+
